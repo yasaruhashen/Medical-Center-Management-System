95f8099658885ffbaa05a2cecfe4e17146129126
Nirushiya <nirushiyamahendrarajah742@gmail.com>
2026-06-11 16:24:20 +0530

    I have created login interface .
    I used figma and WPF to design the login interface.

---
+++ Project_/view/PatientsView.xaml.cs
@@ -0,0 +1,15 @@
+using System.Windows.Controls;
+
+namespace Project_.Views
+{
+    /// <summary>
+    /// Interaction logic for PatientsView.xaml
+    /// </summary>
+    public partial class PatientsView : UserControl
+    {
+        public PatientsView()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Project_/view/ReportsView.xaml.cs
@@ -0,0 +1,15 @@
+using System.Windows.Controls;
+
+namespace Project_.Views
+{
+    /// <summary>
+    /// Interaction logic for ReportsView.xaml
+    /// </summary>
+    public partial class ReportsView : UserControl
+    {
+        public ReportsView()
+        {
+            InitializeComponent();
+        }
+    }
+}
