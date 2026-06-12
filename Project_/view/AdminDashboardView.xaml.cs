98eaec834460107591c4b36977f34b6e9a191922
Hashen Yasaru <129153316+hashenyasaru@users.noreply.github.com>
2026-06-13 01:02:02 +0530

    Admin interface UI components, Admin Login & Authentification
    Designed and implemented the Admin UI in WPF based on the Figma design. Developed navigation functionality and event handling for seamless page transitions. Implemented the Admin Login feature with proper username and password validation to ensure secure authentication. Also added a Logout functionality, allowing administrators to securely sign out of their accounts.

---
+++ Project_/view/AdminDashboardView.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows.Controls;
+
+namespace Project_.Views
+{
+    public partial class AdminDashboardView : UserControl
+    {
+        public AdminDashboardView()
+        {
+            InitializeComponent();
+        }
+    }
+}
